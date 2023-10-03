ec5ca271141079c89a51bdd46e97d435a3a31d89
Eduardo Franco <franc0edu24@gmail.com>
2023-10-02 22:00:19 -0300

    page of account create

---
+++ src/app/pages/account/account.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccountCreateComponent } from './pages/account-create/account-create.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
+
+const routes: Routes = [
+  {
+    path: 'create', component: AccountCreateComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AccountCreateComponent
+  ],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    DividerModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AccountModule { }
---
+++ src/app/pages/account/pages/account-create/account-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-create',
+  templateUrl: './account-create.component.html',
+})
+export class AccountCreateComponent {
+
+}
