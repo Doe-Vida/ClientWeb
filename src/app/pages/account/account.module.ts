cce440b9dff881d33171337352d12f3c0f4612c7
Eduardo Franco <franc0edu24@gmail.com>
2023-10-04 22:16:53 -0300

    login screen ok

--- src/app/pages/account/account.module.ts
+++ src/app/pages/account/account.module.ts
@@ -8,19 +8,23 @@ import { PasswordModule } from 'primeng/password';
 import { DividerModule } from 'primeng/divider';
 import { FormsModule } from '@angular/forms';
 import { PrincipalComponent } from './pages/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: 'create', component: AccountCreateComponent
   },{
     path: 'principal', component: PrincipalComponent
+  },{
+    path: 'login', component: LoginComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AccountCreateComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
