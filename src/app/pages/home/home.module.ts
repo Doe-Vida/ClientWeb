5b3234934dfa1f305e631b5fbaa35e454a558abe
Eduardo Franco <franc0edu24@gmail.com>
2023-10-09 21:55:20 -0300

    header 70%

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
 const routes: Routes = [
   {
@@ -11,7 +13,9 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     CommonModule,
