071c20feadfbb82b11767b7087fd3e75e42d6460
Eduardo Franco <franc0edu24@gmail.com>
2023-10-23 22:01:12 -0300

    implementig button

--- src/app/common/components/button-common/button-common/button-common.component.ts
+++ src/app/common/components/button-common/button-common/button-common.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-common',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./button-common.component.scss']
 })
 export class ButtonCommonComponent {
-
+  @Input() buttonTitle!: string;
 }
