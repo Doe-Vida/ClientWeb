2929abe3b0ca2c0dd0cae5b706e4e834db28d41d
Eduardo Franco <franc0edu24@gmail.com>
2023-11-25 11:37:03 -0300

    starting implementing to maps page

--- src/app/pages/home/pages/home-page/home-page.component.ts
+++ src/app/pages/home/pages/home-page/home-page.component.ts
@@ -8,7 +8,6 @@ import { UserService } from '../shared/user.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit, OnDestroy {
   public _unsubscribe = new Array<Subscription>();
