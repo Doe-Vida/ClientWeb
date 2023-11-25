549d301c6c0bbf99701ec39fe93561fc36a20a32
Eduardo Franco <franc0edu24@gmail.com>
2023-11-25 13:58:10 -0300

    maps page ok

--- src/app/pages/map/pages/map/map.component.ts
+++ src/app/pages/map/pages/map/map.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+  map: any
+  markers: any
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([-22.8683668, -47.0392479], 3);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(this.map);
+
+    //adicionando marcadores
+    this.markers = L.marker([-22.8683668, -47.0392479]).addTo(this.map)
+    .bindPopup('<b>Hospital</b><br>Nome do hospital')
+  }
 }
