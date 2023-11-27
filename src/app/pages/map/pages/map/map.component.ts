1d89ef282dc3b1e83a84b32b6c309d42891a69b0
Eduardo Franco <franc0edu24@gmail.com>
2023-11-26 22:35:07 -0300

    fs can't be used in client is necessary a server

--- src/app/pages/map/pages/map/map.component.ts
+++ src/app/pages/map/pages/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet'
+import { HospitalsMapPositions } from '../../shared/models/hospitals.model';
+import { HospitalsMapPositionService } from '../../shared/service/hospitalsMapPosition.service';
 
 @Component({
   selector: 'app-map',
@@ -9,21 +11,27 @@ import * as L from 'leaflet'
 export class MapComponent implements OnInit{
   map: any
   markers: any
+  hospitals: HospitalsMapPositions[] = new Array<HospitalsMapPositions>();
 
-  constructor() { }
+  constructor(
+    private readonly serviceMock: HospitalsMapPositionService
+  ) { }
 
   ngOnInit(): void {
     this.initMap();
   }
 
   private initMap(): void {
-    this.map = L.map('map').setView([-22.8683668, -47.0392479], 3);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-    }).addTo(this.map);
-
-    //adicionando marcadores
-    this.markers = L.marker([-22.8683668, -47.0392479]).addTo(this.map)
-    .bindPopup('<b>Hospital</b><br>Nome do hospital')
+    this.hospitals = this.serviceMock.getAllHospitalsPosition();
+    this.hospitals.forEach((hospital) => {
+      this.map = L.map('map').setView([-22.8683668, -47.0392479], 3);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+      }).addTo(this.map);
+  
+      //adicionando marcadores
+      this.markers = L.marker([hospital.position.latitude,hospital.position.longitude]).addTo(this.map)
+      .bindPopup(`<b>${hospital.infoWindow.snippet}</b><br>${hospital.infoWindow.title}`)
+    })
   }
 }
---
+++ src/app/pages/map/shared/service/hospitalsMapPosition.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import * as fs from "fs"
+import { HospitalsMapPositions } from "../models/hospitals.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HospitalsMapPositionService{
+    filePath = '../models/hospitals.json';
+
+    getAllHospitalsPosition(): HospitalsMapPositions[]{
+        try{
+            const jsonData = fs.readFileSync(this.filePath, 'utf-8');
+            const hospitals: HospitalsMapPositions[] = JSON.parse(jsonData);
+            return hospitals
+        }catch (error) {
+            console.error('Erro ao ler o arquivo JSON:', error);
+            return []
+        }
+    }
+}
